d3f0a9db5f5f43ad680c71ee1b29db4e6aef9a4c
Opilane TTHK <opilane@tthk.ee>
2021-10-19 10:19:25 +0300

    add button and gallery array

---
+++ app.js
@@ -0,0 +1,14 @@
+const currentImage = document.querySelector('#image');
+const images = ['images/pilt1.jpg','images/pilt2.jpg','images/pilt3.jpg' ];
+
+function changeImage() 
+{
+   console.log('bozo.');
+
+   let randomNumber = Math.floor(Math.random() * images.length);
+
+   console.log(images[randomNumber]);
+
+   currentImage.src = images[randomNumber];
+
+}
